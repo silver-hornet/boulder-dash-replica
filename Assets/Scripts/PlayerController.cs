e48d36f9c410582641b5ad45902460f0daeee60a
silver-hornet <70822422+silver-hornet@users.noreply.github.com>
2020-12-29 18:15:35 -0500

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float moveSpeed;
+    Vector3 pos;
+
+    void Start()
+    {
+        pos = transform.position;
+    }
+
+    void Update()
+    {
+        if (Input.GetAxisRaw("Horizontal") < 0 && transform.position == pos)
+            pos += Vector3.left;
+        if (Input.GetAxisRaw("Horizontal") > 0 && transform.position == pos)
+            pos += Vector3.right;
+        if (Input.GetAxisRaw("Vertical") > 0 && transform.position == pos)
+            pos += Vector3.up;
+        if (Input.GetAxisRaw("Vertical") < 0 && transform.position == pos)
+            pos += Vector3.down;
+
+        transform.position = Vector3.MoveTowards(transform.position, pos, Time.deltaTime * moveSpeed);
+    }
+}
